4535db2a23f1e469b91dd01a7e98e17b637fa297
Ayaan Kakkar <ayan.kakkar@gmail.com>
2025-12-04 23:53:18 -0800

    Add logos

--- frontend/src/components/BrandHeader.tsx
+++ frontend/src/components/BrandHeader.tsx
@@ -1,5 +1,5 @@
 import { Box, Container, Link, Stack, Typography } from "@mui/material";
-import Logo from "../../public/SNPWay_logo.svg";
+import Logo from "../logos/SNPWay_logo.svg";
 
 const BrandHeader = () => {
   return (
